f40a9396768b6eafb805e7378cb06d80fa70aeed
Ulric Huot <ulric.huot@gmail.com>
2023-06-23 21:20:07 -0400

    Update 06.23.2023

---
+++ latest/pkg/client/__env__.js
@@ -0,0 +1,9 @@
+window.WebSocketLink = "ws://localhost:8083/id?3430";
+window.AppData = {
+	URL: "http://localhost:8080",
+	VERSION: 14,
+	DEVELOPERS: "Aloxx & Jakexx & Lann & Peanut (aka арахис)",
+    ENV_MODE: "PROD",
+	
+}
+window.PACKET_MODE = "PROD";
